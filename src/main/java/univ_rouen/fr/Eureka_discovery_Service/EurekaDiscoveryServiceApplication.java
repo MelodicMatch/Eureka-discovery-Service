262f26ec803b683ed184bd680d610a84738bcf65
koubaahm <ahmed.koubaa@univ-rouen.fr>
2024-05-25 16:22:52 +0200

    initialisation micro service

---
+++ src/main/java/univ_rouen/fr/Eureka_discovery_Service/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package univ_rouen.fr.Eureka_discovery_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
